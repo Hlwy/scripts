959067fe20bcdf6b03dd1e91bbea41fa593c0d48
Hunter Young <hunter.lw.young@gmail.com>
2017-12-18 06:43:30 -0600

    fixed directory issue

--- Wifi/Jessie/install_jenssegers_hostapd.sh
+++ Wifi/Jessie/install_jenssegers_hostapd.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get autoremove hostapd
 
-cd ~/Libraries
+cd /home/hunter/Libraries
 wget https://github.com/jenssegers/RTL8188-hostapd/archive/v2.0.tar.gz
 tar -zxvf v2.0.tar.gz
-cd ~/Libraries/RTL8188-hostapd-2.0/hostapd
+cd /home/hunter//Libraries/RTL8188-hostapd-2.0/hostapd
 
 sudo make -j4
 sudo make install
-rm v2.0.tar.gz
+rm /home/hunter/Libraries/v2.0.tar.gz
