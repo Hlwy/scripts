ef4f2f69c4121bf71525c0ead115d26349a20cb1
Hunter Young <hunter.lw.young@gmail.com>
2018-03-02 08:22:16 -0600

    added a script to make setting up ROS network easier

--- config/ROS/RosMachineSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## MAKE SURE THIS IS EXECUTABLE
-#export ROS_HOSTNAME="TheRedtop"
-export ROS_IP="10.192.222.26"
-export ROS_MASTER_URI="http://10.192.222.26:11311"
-export ROSLAUNCH_SSH_UNKNOWN="1"
-##. /opt/ros/distro/setup.sh
-##. ~/your_workspace/devel/setup.sh
-exec "$@"
--- config/ROS/set_ros_ip.sh
+++ config/ROS/set_ros_ip.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
-myip=$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
+# Look for help sign
+if [ "$1" == "-h" ]; then
+  echo "Usage: $0 '$masteruri $myip'"
+  exit 0
+fi
 
-export ROS_IP="$(echo $myip)"
+# check for required argument (assigned static IP address)
+if [[ $# -lt 1 ]];
+	then echo "You need to pass the uri of the ROS Master!  [e.x http://192.168.1.1:11311]"
+	echo "Usage:"
+	echo "sudo $0 master_uri my_ip [hostname]"
+	exit
+fi
+
+# check for required argument (assigned AP SSID)
+if [[ $# -lt 2 ]];
+	then echo "You need to pass the ip associated with this device! [e.x 192.168.1.2]"
+	echo "Usage:"
+	echo "sudo $0 master_uri my_ip [hostname]"
+	exit
+fi
+
+# Store arguments for later usage
+masterUri="$1"
+myIp="$2"
+# hostname="wlan1"
+
+echo "Master Uri: $masterUri"
+echo "My ip: $myIp"
+
+## MAKE SURE THIS IS EXECUTABLE
+export ROS_MASTER_URI="http://$masterUri:11311"
+export ROS_IP=$myIp
+#export ROS_HOSTNAME="TheRedtop"
+export ROSLAUNCH_SSH_UNKNOWN="1"
+
+# myip=$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
+# export ROS_IP="$(echo $myip)"
+
+# exec "$@"
