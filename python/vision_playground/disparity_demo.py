523caaf1a4da3d7e9c7917322553ba0ed37dfeb2
Hunter Young <hunter.lw.young@gmail.com>
2019-02-09 11:13:13 -0500

    added parsed video images, and ipython notebook for quick testing

--- python/vision_playground/disparity_demo.py
+++ python/vision_playground/disparity_demo.py
@@ -1,186 +1,236 @@
 #!/usr/bin/env python
 import cv2, time
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse, pprint
+
+import matplotlib
+from matplotlib import pyplot as plt
 from matplotlib.ticker import NullFormatter
 from matplotlib.transforms import Bbox
-import pprint
+import matplotlib.gridspec as gridspec
 
 pp = pprint.PrettyPrinter(indent=4)
 
-flag_save = 0
-count = 0
+def uvMapping(_img, get_overlay=True,verbose=False):
+    overlay = None
+    h,w = _img.shape
+    histRange = (0,256)
+    histSz = np.max(_img) + 1
+    if(verbose): print("[UV Mapping] Input Image Size: (%d, %d)" % (h,w))
 
-img = cv2.imread("test_disparity.png",cv2.IMREAD_GRAYSCALE)
-h,w = img.shape
-print(img.shape)
+    umap = np.zeros((histSz,w,1), dtype=np.uint8)
+    vmap = np.zeros((h,histSz,1), dtype=np.uint8)
 
-# cv2.namedWindow('Disparity', cv2.WINDOW_NORMAL)
-# cv2.imshow('Disparity', img)
+    for i in range(0,h):
+        vscan = _img[i,:]
+        vrow = cv2.calcHist([vscan],[0],None,[histSz],histRange)
+        if(verbose): print("\t[V Mapping] Scan [%d] (%s) ---- Scan Histogram (%s)" % (i,', '.join(map(str, vscan.shape)), ', '.join(map(str, vrow.shape))))
+        vmap[i,:] = vrow
 
-# cv2.line(histImage, ( bin_w*(i-1), hist_h - int(round(b_hist[i-1])) ),
-#             ( bin_w*(i), hist_h - int(round(b_hist[i])) ),
-#             ( 255, 0, 0), thickness=2)
+    for i in range(0,w):
+        uscan = _img[:,i]
+        urow = cv2.calcHist([uscan],[0],None,[histSz],histRange)
+        if(verbose): print("\t[U Mapping] Scan[%d] (%s) ---- Scan Histogram (%s)" % (i,', '.join(map(str, uscan.shape)), ', '.join(map(str, urow.shape))))
+        umap[:,i] = urow
 
-# histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+    umap = np.reshape(umap,(histSz,w))
+    vmap = np.reshape(vmap,(h,histSz))
 
-histRange = (0,256)
-dmax = np.max(img)
-histSz = dmax+1
-print(dmax)
+    if(get_overlay):
+        blank = np.ones((umap.shape[0],vmap.shape[1]),np.uint8)*255
+        pt1 = np.concatenate((_img, vmap), axis=1)
+        pt2 = np.concatenate((umap,blank),axis=1)
+        overlay = np.concatenate((pt1,pt2),axis=0)
+        overlay = cv2.cvtColor(overlay,cv2.COLOR_GRAY2BGR)
 
-umap = np.zeros((histSz,w,1), dtype=np.uint8)
-vmap = np.zeros((h,histSz,1), dtype=np.uint8)
+    if(verbose):
+        print("\t[UV Mapping] U Map = (%s) ----- V Map = (%s)" % (', '.join(map(str, umap.shape)),', '.join(map(str, vmap.shape)) ))
+    return umap,vmap,overlay
 
-for i in range(0,h):
-    # scan = tImg[i,:]
-    uscan = img[i,:]
-    vscan = img[:,i]
-    # print(uscan.shape)
-    # print(vscan.shape)
-    urow = cv2.calcHist([uscan],[0],None,[histSz],histRange)
-    vrow = cv2.calcHist([vscan],[0],None,[histSz],histRange)
-    # print(scan.ravel())
-    # print(row)
-    # print(
-    # """
+def show_overlay(overlay):
+    plt.imshow(overlay)
+    plt.yticks([''])
+    plt.xticks([''])
+
+def method1(img,angle=120,rho=141,greyThresh=11,houghThresh=5,lineGap=4,lineLength=14,kernelSize=(5,5),show_helpers=False):
+    copy = np.copy(img)
+    tmp = cv2.cvtColor(copy,cv2.COLOR_GRAY2BGR)
+
+    # get current positions of four trackbars
+    n,m,_ = tmp.shape
+    filtered = np.zeros((n,m,3))
+
+    ret, grey = cv2.threshold(copy,greyThresh,255,cv2.THRESH_BINARY)
+
+    kernel = np.ones(kernelSize,np.uint8)
+    closing = cv2.morphologyEx(grey,cv2.MORPH_CLOSE,kernel, iterations = 2)
+
+    ret, helper = cv2.threshold(grey,greyThresh,255,cv2.THRESH_BINARY)
+    ret, helper3 = cv2.threshold(closing,greyThresh,255,cv2.THRESH_BINARY)
+    hlines = helper3
+    ang = angle * np.pi/180
+
+    lines = cv2.HoughLinesP(hlines,rho,ang,houghThresh,lineLength,lineGap)
+    for x in range(0, len(lines)):
+        for x1,y1,x2,y2 in lines[x]:
+            cv2.line(tmp,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.line(filtered,(x1,y1),(x2,y2),(255,255,255),2)
+
+    if(show_helpers):
+        cv2.imshow('threshold grey',grey)
+        cv2.imshow('closed',closing)
+        cv2.imshow('threshold',helper)
+        cv2.imshow('threshold closed',helper3)
+
+    cv2.imshow('Probablistic Lines',tmp)
+    cv2.imshow('Probablistic Filtered',filtered)
+    return filtered
+
+def onChange(pos):
+    global img
+    global tmp
+    show_helpers = False
+    tmp2 = np.copy(img)
+    tmp = cv2.cvtColor(tmp2,cv2.COLOR_GRAY2BGR)
+
+    # tmp3 = cv2.cvtColor(tmp2,cv2.COLOR_BGR2GRAY)
+    # tmp = cv2.cvtColor(tmp2,cv2.COLOR_BGR2GRAY)
+    # tmp = cv2.resize(img, (640,480))
+
+    # get current positions of four trackbars
+    n,m,_ = tmp.shape
+    filtered = np.zeros((n,m,3))
+    e1 = cv2.getTrackbarPos('edgeIn1','image')
+    e2 = cv2.getTrackbarPos('edgeIn2','image')
+    ang2 = cv2.getTrackbarPos('angle','image')
+    rho = cv2.getTrackbarPos('rho','image')
+    minLineLength = cv2.getTrackbarPos('LineLength','image')
+    maxLineGap = cv2.getTrackbarPos('LineGap','image')
+    desiredThresh = cv2.getTrackbarPos('Threshold','image')
+    desiredThresh2 = cv2.getTrackbarPos('Threshold2','image')
+
+    ret, grey = cv2.threshold(tmp2,desiredThresh2,255,cv2.THRESH_BINARY)
+
+    kernel = np.ones((e1,e2),np.uint8)
+    closing = cv2.morphologyEx(grey,cv2.MORPH_CLOSE,kernel, iterations = 2)
+
+    ret, helper = cv2.threshold(grey,desiredThresh2,255,cv2.THRESH_BINARY)
+    ret, helper3 = cv2.threshold(closing,desiredThresh2,255,cv2.THRESH_BINARY)
+    hlines = cv2.Canny(helper3,50,150,apertureSize = 3)
+    ang = ang2 * np.pi/180
+    cv2.imshow('edges',hlines)
+    # print(ang2, ang)
+    lines = cv2.HoughLines(hlines,rho,ang,desiredThresh)
+    count = 0
+    for rho,theta in lines[0]:
+        count+=1
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(tmp,(x1,y1),(x2,y2),(0,0,255),2)
+        cv2.line(filtered,(x1,y1),(x2,y2),(255,255,255),2)
+
+    print(count)
+
+    if(show_helpers):
+        cv2.imshow('threshold grey',grey)
+        cv2.imshow('closed',closing)
+        cv2.imshow('threshold',helper)
+        cv2.imshow('threshold closed',helper3)
+    cv2.imshow('lines',tmp)
+    cv2.imshow('filtered',filtered)
+
+def crap1():
+    pp = pprint.PrettyPrinter(indent=4)
+
+    cv2.namedWindow('Disparity', cv2.WINDOW_NORMAL)
+    cv2.imshow('Disparity', img)
+
+    cv2.line(histImage, ( bin_w*(i-1), hist_h - int(round(b_hist[i-1])) ),
+                ( bin_w*(i), hist_h - int(round(b_hist[i])) ),
+                ( 255, 0, 0), thickness=2)
+
+    histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+
+def crap2():
+    new_dimg = np.zeros((h,w,1), dtype=np.uint8)
+    new_dimg2 = np.zeros((h,w,1), dtype=np.uint8)
+
+    for i in range(0,h):
+        asa = cv2.calcBackProject([img[i,:]],[0],umap[:,i],histRange,1)
+        new_dimg[i,:] = asa
+
+    for i in range(0,h):
+        asa = cv2.calcBackProject([img[:,i]],[0],vmap[i,:],histRange,1)
+        new_dimg2[:,i] = asa
+
+        print(new_dimg2.shape)
+
+
+    cv2.namedWindow('Disparity', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Vmap', cv2.WINDOW_NORMAL)
+    cv2.imshow('Disparity', new_dimg)
+    cv2.imshow('Vmap', new_dimg2)
+
+    while True:
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+# cv2.imwrite("test_umap_tmp.png",umap)
+# cv2.imwrite("test_vmap_tmp.png",vmap)
+
+
+if __name__ == "__main__" :
+
+    # Setup commandline argument(s) structures
+    ap = argparse.ArgumentParser(description='Image Segmentation')
+    ap.add_argument("--pic", "-p", type=str, default="test_disparity.png", metavar='FILE', help="Name of video file to parse")
+    # Store parsed arguments into array of variables
+    args = vars(ap.parse_args())
+
+    # Extract stored arguments array into individual variables for later usage in script
+    _img = args["pic"]
+
+    # create trackbars for color change
+    cv2.namedWindow('image')
+    # cv2.namedWindow('edges')
+    cv2.createTrackbar('edgeIn1','image',5,1000,onChange)
+    cv2.createTrackbar('edgeIn2','image',5,1000,onChange)
+    cv2.createTrackbar('rho','image',141,255,onChange)
+    cv2.createTrackbar('angle','image',120,180,onChange)
+    cv2.createTrackbar('LineLength','image',14,255,onChange)
+    cv2.createTrackbar('LineGap','image',4,255,onChange)
+    cv2.createTrackbar('Threshold','image',5,255,onChange)
+    cv2.createTrackbar('Threshold2','image',11,255,onChange)
+
+    img = cv2.imread(_img,cv2.IMREAD_GRAYSCALE)	# Input video file as OpenCV VideoCapture device
+    # img = cv2.imread(_img)	# Input video file as OpenCV VideoCapture device
+    # plt.ion()
+    umap, vmap, overlay = uvMapping(img)
+
+
+    # tmp = np.copy(img)
+    # tmp = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    # h,w = img.shape
+    # print(img.shape)
     #
-    # ==========================
+    # flag_save = 0
+    # count = 0
     #
-    # """)
-    umap[:,i] = urow
-    vmap[i,:] = vrow
-
-tmp = np.reshape(umap,(histSz,w))
-tmpw = np.reshape(vmap,(h,histSz))
-
-# print(umap.shape)
-# print(tmp.shape)
-
-
-new_dimg = np.zeros((h,w,1), dtype=np.uint8)
-new_dimg2 = np.zeros((h,w,1), dtype=np.uint8)
-
-# asa = cv2.calcBackProject([img],[0],umap[:,0],histRange,1)
-for i in range(0,h):
-    asa = cv2.calcBackProject([img[i,:]],[0],umap[:,i],histRange,1)
-    new_dimg[i,:] = asa
-
-for i in range(0,h):
-    asa = cv2.calcBackProject([img[:,i]],[0],vmap[i,:],histRange,1)
-    new_dimg2[:,i] = asa
-
-    print(new_dimg2.shape)
-# asa2 = cv2.calcBackProject([img],[0],vmap,histRange,1)
-
-
-cv2.namedWindow('Disparity', cv2.WINDOW_NORMAL)
-cv2.namedWindow('Vmap', cv2.WINDOW_NORMAL)
-cv2.imshow('Disparity', new_dimg)
-cv2.imshow('Vmap', new_dimg2)
-
-while True:
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-fig, axs = plt.subplots(2, 2)
-nullfmt = NullFormatter()
-
-axs[0,0].imshow(img)
-axs[0,1].imshow(tmpw)
-axs[1,0].imshow(tmp)
-axs[1,1].imshow(asa)
-for ax in axs.flatten():
-    ax.set_xticklabels('')
-    ax.set_yticklabels('')
-    ax.set_frame_on(0)
-    ax.xaxis.set_major_formatter(nullfmt)
-    ax.xaxis.set_ticks_position('none')
-    ax.yaxis.set_major_formatter(nullfmt)
-    ax.yaxis.set_ticks_position('none')
-    # pp.pprint(ax.properties())
-    # print(
-    # """
     #
-    # ==========
-    #
-    # """
-    # )
-fig.set_constrained_layout_pads(w_pad=0., h_pad=0., hspace=0., wspace=0.)
+    while True:
+        key = cv2.waitKey(5) & 0xFF
+        cv2.imshow("image", overlay)
+        if key == ord('q'):
+            break
+        # show_overlay(overlay)
+        # plt.show()
+        # plt.pause(0.01)
 
-# print(axs[0,0].get_position(True))
-# print(axs[0,1].get_position(True))
-# print(axs[1,0].get_position(True))
-# print(axs[1,1].get_position(True))
-
-dx2 = abs(0.547727272727 - 0.9)
-dy3 = abs(0.11 - 0.46)
-
-bx2 = Bbox([[0.477272727273,0.53],[0.477272727273+dx2,0.88]])
-axs[0,1].set_position(bx2, 'active')
-
-# axs[1,0].imshow(tmp)
-
-# 'position': Bbox([[0.125, 0.53], [0.477272727273, 0.88]]),
-# 'window_extent': Bbox([[76.5, 250.9], [308.954545455, 425.9]]),
-
-# 'position': Bbox([[0.547727272727, 0.53], [0.9, 0.88]]),
-# 'window_extent': Bbox([[347.045454545, 250.9], [579.5, 425.9]]),
-# 'window_extent': Bbox([[308.954545455, 250.9], [579.5, 425.9]]),
-
-# 'position': Bbox([[0.125, 0.11], [0.477272727273, 0.46]]),
-# 'window_extent': Bbox([[76.5, 49.3], [308.954545455, 224.3]]),
-
-# 'position': Bbox([[0.547727272727, 0.11], [0.9, 0.46]]),
-# 'window_extent': Bbox([[347.045454545, 49.3], [579.5, 224.3]]),
-
-
-# fig.subplots_adjust(hspace=0)
-
-# cv2.imshow('Disparity', umap)
-# left, width = 0.1, 0.65
-# bottom, height = 0.1, 0.65
-# bottom_h = left_h = left + width + 0.02
-#
-# rectd = [left, bottom, width, height]
-# rectv = [left, bottom_h, width, 0.2]
-# rectu = [width+ 0.02, bottom, 0.2, height]
-#
-# nullfmt = NullFormatter()
-# fig = plt.figure(1, figsize=(8, 8))
-#
-# axd = plt.axes(rectd,sharex=True,sharey=True)
-# axv = plt.axes(rectv,sharex=True,sharey=True)
-# axu = plt.axes(rectu,sharex=True,sharey=True)
-# fig.add_axes(axd)
-# fig.add_axes(axv)
-# fig.add_axes(axu)
-#
-# axv.xaxis.set_major_formatter(nullfmt)
-# axu.yaxis.set_major_formatter(nullfmt)
-
-# fig, axs = plt.subplots(2,2,sharex=True,sharey=True)
-# fig.subplots_adjust(hspace=0,wspace=0)
-
-# axd.imshow(img)
-# axu.imshow(tmpw)
-# axv.imshow(tmp)
-
-# axd.set_aspect('equal')
-# axu.set_aspect('equal')
-# axv.set_aspect('equal')
-
-# axs[0,0].imshow(img)
-# axs[0,1].imshow(tmpw)
-# axs[1,0].imshow(tmp)
-
-
-# plt.subplot(2,2,2)
-# plt.subplot(2,2,3)
-plt.show()
-cv2.imwrite("test_umap_tmp.png",umap)
-cv2.imwrite("test_vmap_tmp.png",vmap)
-
-while True:
-    if cv2.waitKey(1) == ord('q'):
-        break
+    cv2.destroyAllWindows()
